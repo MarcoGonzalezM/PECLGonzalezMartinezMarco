f7ee015787347504a934c61364c8d935ad8046ff
Silvana <56298562+silvanadct@users.noreply.github.com>
2021-10-26 17:56:44 +0200

    Add files via upload

--- Controlador.cpp
+++ Controlador.cpp
@@ -1,26 +1,44 @@
 #include "Controlador.hpp"
-#include "Paciente.cpp"
+#include "Paciente.hpp"
 #include "iostream"
+#include "Cola.hpp"
+#include "Pila.hpp"
+
 Controlador::Controlador()
 {
 }
 void Controlador::genera12Pacientes()
 {
 	for(int i=0; i<12; i++) {
-		Paciente p = new Paciente;
-		p->Pila.insertar;
+		Paciente* p = new Paciente;
+		p->Pila.insertar();
 		}
-	cout << "\t Pacientes en la pila -> " << Pila.longitud << "\n Pacientes en las colas:" << setw(20) << "\n Sala A-> "
-	<< /*Cola.longitud(A) <<*/ setw(20) << "Sala B-> " << /*Cola.longitud(B) <<*/ setw(20) << "Sala C-> " /*<< Cola.longitud(C)*/
+	cout << "\t Pacientes en la pila -> " << Pila.getLongitud() << "\n Pacientes en las colas:" << setw(20) << "\n Sala A-> "
+	<< /*Cola.getLongitud(A) <<*/ setw(20) << "Sala B-> " << /*Cola.longitud(B) <<*/ setw(20) << "Sala C-> " /*<< Cola.longitud(C)*/
 	<< setw(20) << "Sala D-> " /*<< Cola.longitud(D)*/ << "\n Pacientes en las listas: \n" << setw(30) << "Quirofano "
-	"Apendicitis-> " /*<< Lista.longitudA */<< setw(30) << "Quirofano hernias-> " */<< Lista.longitudH*/ << "\n Pacientes "
+	"Apendicitis-> " /*<< Lista.longitudA */<< setw(30) << "Quirofano hernias-> " /*<< Lista.longitudH*/ << "\n Pacientes "
 	"en el arbol -> " /*<< arbolLongitud */<< endl;
 	}
-	
+
 void Controlador::muestraPacientes()
 {
 	Paciente.mostrar();
 }
+
+void Controlador::borraPacientesPila()
+{
+	int n=Pila.getLongitud()
+	for(int i=0; i<n; i++){
+		Pila.extraer();
+		}
+}
+
+void Controlador::encolarPacientes()
+{
+	
+	}
+}
+
 Controlador::~Controlador()
 {
 }
