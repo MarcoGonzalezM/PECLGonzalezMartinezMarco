cc3a4605b8fdb2b21ccc5c4f23dcc2c6e8ed8e77
Silvana <56298562+silvanadct@users.noreply.github.com>
2021-11-09 17:26:59 +0100

    Add files via upload

--- Lista.cpp
+++ Lista.cpp
@@ -1,8 +1,65 @@
 #include "Lista.hpp"
 
-Lista::Lista()
+Lista::Lista() //las listas tienen que hacerse ordenadas
 {
+	cabeza = NULL;
+	ultimo = NULL;
+	longitud = 0;
 }
+
+Paciente* Lista::insertar(Paciente v*){    //INSERTAR EN ORDEN
+	pnodoLista nuevo;
+	nuevo = new NodoLista(v);
+	if (primero)
+		if (v<primero)
+			primero = v;
+		else{
+			pnodoLista aux = primero;
+			while(aux<v) {
+				aux = aux->siguiente;
+				
+			}
+		}
+		ultimo->siguiente = nuevo;
+	ultimo = nuevo;
+	else (!primero)
+		primero = nuevo;
+	longitud++;
+}
+
+Paciente* Lista::extraer(){
+	pnodoLista nodo;
+	Paciente* v;
+	nodo = primero;        //los pacientes van a entrar por prioridad y ya estan ordenados al entrar en lista
+    if (!nodo)
+        return NULL;
+    primero = nodo->siguiente;
+    v = nodo->valor;
+    delete nodo;
+    if (!primero)
+        ultimo = NULL;    
+    longitud--;
+    return v;
+}
+
+void Lista::mostrar(){
+	pnodoLista aux = primero;
+	cout << "\tLos pacientes en lista: ";
+	while(aux) {
+		cout << " -> " << aux->valor;
+		aux = aux->siguiente;
+	}
+	cout << endl;
+}
+
+Paciente* Lista::cabeza(){
+	pnodoLista nodo;
+	if (!primero)
+		return 0;
+	return primero->valor;
+}
+
+
 
 Lista::~Lista()
 {
